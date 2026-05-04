a2ee3c85913c7177bc20d7aa925f185bcf63437c
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-04 11:38:30 +0700

    menambah fungsi untuk cetak data

--- destructor.cpp
+++ destructor.cpp
@@ -18,6 +18,15 @@ angka::angka(int i){
     arr = new int[i];
     isiData();
 }
+
+angka::~angka()
+{
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat array sudah dilepaskan." << endl;
+}
+
 int main()
 {
     return 0;

47063e4e2ca9b230c27f43ac584ed0161dd231d9
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-04 12:44:14 +0700

    menampilkan nim, nama serta mengisi fungsi main

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -26,8 +26,22 @@ mahasiswa::mahasiswa(string iNama){
 mahasiswa::mahasiswa(int iNim, string iNama){
     nim = iNim; //definisi nim dan nama
     nama = iNama;
-};
+}
+
+void mahasiswa::cetak(){
+  cout << endl << "NIM: " << nim << endl;
+  cout << "Nama: " << nama << endl;
+}
+
 int main()
 {
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("Andi");
+    mahasiswa mhs3(010203,"santi");
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1: "; mhs1.cetak();
+    cout << "Mahasiswa 2: "; mhs2.cetak();  
+    cout << "Mahasiswa 3: "; mhs3.cetak();
     return 0;
 }

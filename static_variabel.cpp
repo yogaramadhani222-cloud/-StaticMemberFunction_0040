75348b4a0c80cf89171acd11084646ab64a7e125
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-05-04 12:10:23 +0700

    menambahkan deklarasi prosedur set id dan print all serta membuat constructor mahasiswa

--- static_variabel.cpp
+++ static_variabel.cpp
@@ -8,4 +8,14 @@ class mahasiswa
     //deklarasi variabel member
     int id;
     string nama;
+
+    //deklarasi prosedur setID() dan printAll()
+    void setID();
+    void printAll();
+
+    //pembuatan constructor mahasiswa dengan parameter
+    mahasiswa(string pnama) : nama(pnama) 
+    {
+        setID();
+    }
 };
